a0ac37c66bf583d2628050864bb1cc82bf9b2a50
DNParadox <m96francesco@gmail.com>
2022-08-22 13:01:24 +0200

    Test push in branch

---
+++ database.php
@@ -0,0 +1,61 @@
+<?php
+
+    $database = [
+        [
+            'title' => '',
+            'author' => '',
+            'year' => '',
+            'cover' => '',
+            'genre' => '',
+        ],
+        [
+            'title' => '',
+            'author' => '',
+            'year' => '',
+            'cover' => '',
+            'genre' => '',
+        ],
+        [
+            'title' => '',
+            'author' => '',
+            'year' => '',
+            'cover' => '',
+            'genre' => '',
+        ],
+        [
+            'title' => '',
+            'author' => '',
+            'year' => '',
+            'cover' => '',
+            'genre' => '',
+        ],
+        [
+            'title' => '',
+            'author' => '',
+            'year' => '',
+            'cover' => '',
+            'genre' => '',
+        ],
+        [
+            'title' => '',
+            'author' => '',
+            'year' => '',
+            'cover' => '',
+            'genre' => '',
+        ],
+        [
+            'title' => '',
+            'author' => '',
+            'year' => '',
+            'cover' => '',
+            'genre' => '',
+        ],
+        [
+            'title' => '',
+            'author' => '',
+            'year' => '',
+            'cover' => '',
+            'genre' => '',
+        ],
+    ];
+?>
