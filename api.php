6fd3cee6675310ef4a407d79406e6450debfe6d7
zhLisha <lisazhou1709@gmail.com>
2022-08-22 16:13:16 +0200

    Filter genre API

--- api.php
+++ api.php
@@ -1,8 +1,20 @@
 <?php
     require __DIR__ . '/database.php';
+    
 
+    if( empty($_GET['genre']) || $_GET['genre'] === 'all' ) {
+        header('Content-Type: application/json');
+        echo json_encode($database);
+    } else {
+        $filteredGenre = [];
 
-    header('Content-Type: application/json');
-
-    echo json_encode($database);
+        foreach($database as $genre) {
+            if($genre['genre'] === $_GET['genre']) {
+                $filteredGenre = $genre;
+            }
+        }
+        
+        header('Content-Type: application/json');
+        echo json_encode($filteredGenre);
+    }
 ?>
