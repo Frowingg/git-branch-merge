8992592c004ca40f76d4bf7e4616813b92db97f5
zhLisha <lisazhou1709@gmail.com>
2022-08-22 15:49:07 +0200

    Added filtered genre option

--- api.php
+++ api.php
@@ -1,8 +1,22 @@
 <?php
     require __DIR__ . '/database.php';
+    
+
+    if( empty($_GET['genre']) || $_GET['genre'] === 'all' ) {
+        header('Content-Type: application/json');
+        echo json_encode($database);
+    } else {
+        $filteredGenre = [];
+
+        foreach($database as $genre) {
+            if($genre['genre'] === $_GET['genre']) {
+                $filteredGenre = $genre;
+            }
+        }
+        
+        header('Content-Type: application/json');
+        echo json_encode($filteredGenre);
+    }
 
 
-    header('Content-Type: application/json');
-
-    echo json_encode($database);
 ?>
